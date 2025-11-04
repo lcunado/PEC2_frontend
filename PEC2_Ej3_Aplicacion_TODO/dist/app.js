ad98b94163c477b25c87648088e719f590d27e98
[Laura Cuñado García] <[lcunado@uoc.edu]>
2025-11-04 10:34:51 +0100

    Finalizo PEC2

--- PEC2_Ej3_Aplicacion_TODO/dist/app.js
+++
@@ -1,6 +0,0 @@
-import { TodoService } from './services/todo.service';
-import { TodoView } from './views/todo.views';
-import { TodoController } from './controllers/todo.controller';
-// Punto de entrada de la aplicación
-// Crea las instancias y lanza el controlador
-const app = new TodoController(new TodoService(), new TodoView());
